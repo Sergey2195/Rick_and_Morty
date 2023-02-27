af9b827ac94367ce9c2cdba47a2c547697e5267e
serser <Sergey21.95@gmail.com>
2023-02-28 00:27:45 +0300

    add collapsing toolbar

--- app/src/main/java/com/aston/rickandmorty/MainActivity.kt
+++ app/src/main/java/com/aston/rickandmorty/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.aston.rickandmorty
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.FragmentContainerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.mainToolBar))
+        supportActionBar?.setTitle("kekw")
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportFragmentManager.beginTransaction()
+            .add(R.id.mainFragmentContainer, MainScreenFragment.newInstance("", ""))
+            .commit()
     }
 }
