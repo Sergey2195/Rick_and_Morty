22a528e178486982dee046b48cd0e47933457b37
serser <Sergey21.95@gmail.com>
2023-02-28 16:36:17 +0300

    add splash screen

---
+++ app/src/main/java/com/aston/rickandmorty/presentation/activities/SplashScreenActivity.kt
@@ -0,0 +1,28 @@
+package com.aston.rickandmorty.presentation.activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.ImageView
+import com.aston.rickandmorty.R
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        val image = findViewById<ImageView>(R.id.splashScreenImageView)
+        Glide.with(this).asGif()
+            .load(R.drawable.three_seconds_rick)
+            .apply(RequestOptions.diskCacheStrategyOf(DiskCacheStrategy.NONE))
+            .into(image)
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 2000L)
+    }
+}
