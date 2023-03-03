ae67d2d5ca79c2acfc8caa743288f60a5d1c7065
serser <Sergey21.95@gmail.com>
2023-03-03 09:06:30 +0300

    add loading images

--- app/src/main/java/com/aston/rickandmorty/presentation/viewHolders/CharacterViewHolder.kt
+++ app/src/main/java/com/aston/rickandmorty/presentation/viewHolders/CharacterViewHolder.kt
@@ -1,8 +1,10 @@
 package com.aston.rickandmorty.presentation.viewHolders
 
 import androidx.recyclerview.widget.RecyclerView
+import com.aston.rickandmorty.R
 import com.aston.rickandmorty.databinding.CharacterItemBinding
 import com.aston.rickandmorty.domain.entity.CharacterModel
+import com.bumptech.glide.Glide
 
 class CharacterViewHolder(
     private val binding: CharacterItemBinding
@@ -10,6 +12,10 @@ class CharacterViewHolder(
 
     fun populate(data: CharacterModel, clickListener: ((id:Int)-> Unit)?) {
         //TODO binding.characterImage.setImageDrawable()
+        Glide.with(binding.root)
+            .load(data.image)
+            .placeholder(R.drawable.toolbar_image)
+            .into(binding.characterImage)
         binding.characterNameValue.text = data.name
         binding.characterGenderValue.text = data.gender
         binding.characterSpeciesValue.text = data.species
--- app/src/main/java/com/aston/rickandmorty/presentation/viewModels/CharactersViewModel.kt
+++ app/src/main/java/com/aston/rickandmorty/presentation/viewModels/CharactersViewModel.kt
@@ -5,14 +5,10 @@ import androidx.lifecycle.viewModelScope
 import androidx.paging.PagingData
 import androidx.paging.cachedIn
 import com.aston.rickandmorty.data.CharacterRepositoryImpl
-import com.aston.rickandmorty.data.CharactersRepository
 import com.aston.rickandmorty.domain.entity.CharacterModel
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
 
-class CharactersViewModel: ViewModel() {
+class CharactersViewModel : ViewModel() {
     val charactersFlow: Flow<PagingData<CharacterModel>>
     val tmpRepository = CharacterRepositoryImpl()
 
@@ -20,7 +16,7 @@ class CharactersViewModel: ViewModel() {
         charactersFlow = tmpRepository.getFlowAllCharacters().cachedIn(viewModelScope)
     }
 
-    fun updateData(){
+    fun updateData() {
         //todo
     }
 }
